db783276e10974e1e55e4b1d6d647d84291b894a
Дмитрий Д <divirdima@gmail.com>
2022-06-08 16:32:11 +0300

    v.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+    	AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("world beans: " + (bean1 == bean2));
+        
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println("cat's bean: " + (bean3 == bean4));
+        
+        applicationContext.close();
     }
 }
